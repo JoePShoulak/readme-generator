5b3b33404316c7e463aadc88472ce2f22be411a8
Joe P Shoulak <joe@squarecompass.io>
2022-10-07 16:46:11 -0500

    ready to publish

--- index.js
+++ index.js
@@ -7,17 +7,20 @@ import questions from "./lib/questions.js";
 // We need this to correctly access our process.env variables
 dotenv.config();
 
-// We're using inquirer to grab our data
-inquirer
-	.prompt(questions) // Ask the questions
-	.then((answers) => { // Get the answers
-		let readme = new ReadMe(answers); // Generate the readme content and save it out
-		readme.save();
-	})	
-	.catch((error) => {
-		if (error.isTtyError) {
-			console.errorlog("Prompt couldn't be rendered in the current environment");
-		} else {
-			console.error(error);
-		}
-	});
+async function getAnswers(questions) {
+	return await inquirer.prompt(questions);
+}
+
+async function init() {
+	let response = {license: "MIT"};
+
+	// TODO: Improve CLI arg support
+	if (process.argv.length == 2) {
+		response = await getAnswers(questions);
+	}
+
+	const readme = new ReadMe(response);
+	readme.save();
+}
+
+init()
